54d04247c2f0d8b616e358d426cb909861048083
Suvo-Paul <suvopaul098@gmail.com>
2024-09-22 16:45:18 +0530

    ncncng

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 dotenv.config()
 
